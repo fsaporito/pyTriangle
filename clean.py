d4460d3a37d988c7830f3f3a911c9f6aa1cae4ce
fsaporito <f.saporito2@campus.unimib.it>
2018-02-05 14:45:36 +0100

    Removed py_cache files

--- clean.py
+++ clean.py
@@ -3,5 +3,8 @@ import os
 # Make Clean
 os.system("cd triangle && make distclean && cd ..")
 os.system("cd proto && make clean && cd ..")
+os.system("cd proto && make clean && cd ..")
+os.system("cd __pycache__ && rm -f * && cd ..")
+os.system("cd protopy && cd __pycache__ && rm -f * && cd .. && cd ..")
 os.system("rm -f triangle.bin")
 os.system("rm -f showme.bin")
